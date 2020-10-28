9cb7fed721c5c8448b25521ddda3371fb1835e38
Karol Belina <karolbelina@gmail.com>
2020-10-28 04:40:52 +0100

    Add the intro to the Chomsky Hierarchy

---
+++ listings/ast.hs
@@ -0,0 +1,12 @@
+type Grammar    = [Production]
+type Production = (String, Expression)
+data Expression = Alternative Expression Expression [Expression]
+                | Sequence Expression Expression [Expression]
+                | Optional Expression
+                | Repeated Expression
+                | Factor Integer Expression
+                | Exception Expression Expression
+                | Nonterminal String
+                | Terminal String
+                | Special String
+                | Empty

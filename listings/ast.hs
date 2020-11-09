c75641f12291bd6f5d7351d2a79c1061f77366f5
Karol Belina <karolbelina@gmail.com>
2020-11-09 18:20:30 +0100

    Add the domain modelling section

--- listings/ast.hs
+++ listings/ast.hs
@@ -1,11 +1,21 @@
 type Grammar    = [Production]
 type Production = (String, Expression)
-data Expression = Alternative Expression Expression [Expression]
-                | Sequence Expression Expression [Expression]
+data Expression = Alternative { first :: Expression
+                              , second :: Expression
+                              , rest :: [Expression]
+                              }
+                | Sequence { first :: Expression
+                           , second :: Expression
+                           , rest :: [Expression]
+                           }
                 | Optional Expression
                 | Repeated Expression
-                | Factor Integer Expression
-                | Exception Expression Expression
+                | Factor { count :: Integer
+                         , primary :: Expression
+                         }
+                | Exception { subject :: Expression
+                            , restriction :: Expression
+                            }
                 | Nonterminal String
                 | Terminal String
                 | Special String
---
+++ listings/token.hs
@@ -0,0 +1,16 @@
+data Token = Nonterminal String
+           | Terminal String
+           | Special String
+           | Integer Integer
+           | Concatenation
+           | Definition
+           | DefinitionSeparator
+           | EndGroup
+           | EndOption
+           | EndRepeat
+           | Exception
+           | Repetition
+           | StartGroup
+           | StartOption
+           | StartRepeat
+           | Terminator

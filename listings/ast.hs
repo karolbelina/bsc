8dacb67bf0168b352927df3e183903180a76bd6e
Karol Belina <karolbelina@gmail.com>
2020-11-14 16:06:55 +0100

    Add sections for Regex101 and Pest

--- listings/ast.hs
+++ listings/ast.hs
@@ -1,22 +1,11 @@
-type Grammar    = [Production]
-type Production = (String, Expression)
-data Expression = Alternative { first :: Expression
-                              , second :: Expression
-                              , rest :: [Expression]
-                              }
-                | Sequence { first :: Expression
-                           , second :: Expression
-                           , rest :: [Expression]
-                           }
-                | Optional Expression
-                | Repeated Expression
-                | Factor { count :: Integer
-                         , primary :: Expression
-                         }
-                | Exception { subject :: Expression
-                            , restriction :: Expression
-                            }
-                | Nonterminal String
-                | Terminal String
-                | Special String
-                | Empty
+data Expression
+  = Alternative Expression Expression [Expression]
+  | Sequence Expression Expression [Expression]
+  | Optional Expression
+  | Repeated Expression
+  | Factor Integer Expression
+  | Exception Expression Expression
+  | Nonterminal String
+  | Terminal String
+  | Special String
+  | Empty
--- listings/token.hs
+++ listings/token.hs
@@ -1,16 +1,17 @@
-data Token = Nonterminal String
-           | Terminal String
-           | Special String
-           | Integer Integer
-           | Concatenation
-           | Definition
-           | DefinitionSeparator
-           | EndGroup
-           | EndOption
-           | EndRepeat
-           | Exception
-           | Repetition
-           | StartGroup
-           | StartOption
-           | StartRepeat
-           | Terminator
+data Token
+  = Nonterminal String
+  | Terminal String
+  | Special String
+  | Integer Integer
+  | Concatenation
+  | Definition
+  | DefinitionSeparator
+  | EndGroup
+  | EndOption
+  | EndRepeat
+  | Exception
+  | Repetition
+  | StartGroup
+  | StartOption
+  | StartRepeat
+  | Terminator

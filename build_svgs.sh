2759ab22fc1fa9a2e233eddca59d347f6ee6d606
Karol Belina <karolbelina@gmail.com>
2021-01-10 07:43:50 +0100

    Fix the indentation

--- build_svgs.sh
+++ build_svgs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for path in $(find ./images -name '*.svg'); do
-	filename="${path##*/}"
-	basename="${filename%%.*}"
+    filename="${path##*/}"
+    basename="${filename%%.*}"
     inkscape --export-latex --export-filename=images/$basename.pdf images/$basename.svg
 done
